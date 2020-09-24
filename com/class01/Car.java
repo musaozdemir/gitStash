d7744e18b9ae3f21c1e5a1ee420c147bba5f91f2
musaozdemir <musaozdemir@me.com>
2020-09-24 19:33:09 -0400

    modified class01 committed to local repros...

--- com/class01/Car.java
+++ com/class01/Car.java
@@ -2,6 +2,11 @@ package com.class01;
 
 public class Car {
 	public static void main(String[] args) {
+		
+		System.out.println("i want to buy a car");
 
+		
+		System.out.println("stash den sonra olusturdugum!!!!");
+		
 	}
 }

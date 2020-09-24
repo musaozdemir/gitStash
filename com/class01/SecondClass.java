b75b85d33cabe87f985ca0f13b4a07e9d6ef291c
musaozdemir <musaozdemir@me.com>
2020-09-24 19:57:40 -0400

    second class modified

--- com/class01/SecondClass.java
+++ com/class01/SecondClass.java
@@ -4,5 +4,7 @@ public class SecondClass {
 
 	void div (int a, int b) {
 		System.out.println(a/b);
+		
+		System.out.println("su an bukismi ekliyorum sonra stash den sonra mauel olarak second class i silicem. sonra stash back yapicam");
 	}
 }
